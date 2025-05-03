d778d3c0faf29211a10e409ca6b4892c8e59ee29
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-04 02:04:56 +0600

    java

--- Math Class/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public class static void main(String[] args){
-        //the value of pi
-        System.out.println(Math.PI);
-
-    }
-}
---
+++ mathClass/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println(Math.PI); //3.141592653589793
+    }
+}
