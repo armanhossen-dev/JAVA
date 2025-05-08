d6b8a7915039b11edd7e36a9204a7e1bdddc69f2
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-08 22:50:35 +0600

    java

---
+++ for_loop/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        //https://youtu.be/xTtL8E4LzTQ?t=13419
+        //for loop = execute some code a CERTAIN amount of times
+        /*
+        for(;;){
+
+        */
+    }
+}
