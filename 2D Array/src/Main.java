fb01345654ddf0698c0d4f306c07c2fa6a946507
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-15 01:31:51 +0600

    java

--- 2D Array/src/Main.java
+++ 2D Array/src/Main.java
@@ -28,5 +28,53 @@ public class Main {
         Potato Onion Carrot
         Chicken Beef Fish
          */
+        System.out.println();
+        // to change the elements value
+        groceries[0][0] = "Pineapple";
+        /*
+        0 1 2 3
+        1
+        2
+        3
+        */
+        for(String[] foods : groceries){
+            //System.out.println(foods); // we will get 3 arrays (fruits, vegetables, meats) first elements address
+            for(String name : foods){
+                System.out.print(name + " ");
+            }
+            System.out.println();
+        }
+        /*
+        Pineapple Orange Banana
+        Potato Onion Carrot
+        Chicken Beef Fish
+         */
+
+        //Creating telephone number pad
+        /*
+        1 2 3
+        4 5 6
+        7 8 9
+        * 0 #
+         */
+        System.out.println();
+        int[][] numPad = {{1,2,3},
+                {4,5,6}, {7,8,9}}; // but we can no use char, {'*', 0, '#'} so we have to use char 2D array
+        char[][] telephoneNums = {{'1','2','3'},
+                                  {'4','5','6'},
+                                  {'7','8','9'},
+                                  {'*', '0', '#'}};
+        for(char[] row : telephoneNums){
+            for(char num : row){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
     }
+
 }
