f58be287920c48c72c75e74762600f34727cd01b
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-16 04:40:35 +0600

    OOP

---
+++ Object/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        //https://youtu.be/xTtL8E4LzTQ?t=24120
+        /*
+        # in programming an objcet  can represent an real-world entity
+
+        object = an entity that hold data (attributes), can have things
+                and can perform actions (methods), can do things
+                it is a reference data type
+        - we store the data for object,  in a location known as heap
+
+        lets create some car objects
+        - we will need class,
+            class -  A class can serve/behave as a blueprint to create objects
+            'src' - 'new' - 'java class' - name it 'car' then enter
+         */
+    }
+}
