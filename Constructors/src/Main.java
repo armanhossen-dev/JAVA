7c5ca6d2d45887c14ab910f932fa1df8e4b2ef48
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-19 02:02:54 +0600

    java

--- Constructors/src/Main.java
+++ Constructors/src/Main.java
@@ -23,17 +23,18 @@ public class Main {
 
         /////////////////////////////////////////////////////////////////
         // Calling a constructor when we instantiate an object of that class
+        // a constructor is automatically called when a object is created
 
         Student stu1 = new Student();
+
                      //instantiate an object '= new Student();'
         // here constructor -> 'Student()', in this () we can pass arguments
+        */
 
-         */
         //ip = input
         Student stu1 = new Student("Arman Hossen", 22, 2,3.12);
         // System.out.println("stu1.name = " + stu1.name); //stu1.name = Arman Hossen
         Student stu2 = new Student("Jannatul Ritu", 24, 1, 3.89);
-
         System.out.println("stu1 = " + stu1.name + ", " + stu1.age + ", " + stu1.roll + ", " + stu1.cgpa + ", " + stu1.isEnrolled);
         System.out.println("stu2 = " + stu2.name + ", " + stu2.age + ", " + stu2.roll + ", " + stu2.cgpa + ", " + stu2.isEnrolled);
 
