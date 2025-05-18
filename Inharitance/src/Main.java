b1a7129466de8c874e06adf6395ddea493c02b4b
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-05-19 04:42:36 +0600

    java

---
+++ Inharitance/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //https://youtu.be/xTtL8E4LzTQ?t=26533
+    public static void main(String[] args){
+        // Inheritance = one class inherits the attributes and methods from another class
+        //               Child <- Parent
+
+
+
+
+    }
+}
