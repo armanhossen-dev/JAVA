f6401bdda4e5d3a407d6e44fb2e58897106ec582
Arman Hossen <2armanhossen@gmail.com>
2025-06-18 02:15:19 +0600

    Practice
    OOP Varsity Tasks

--- Problem 6 Print Odd Numbers for loop continue/src/Main.java
+++ Problem 6 Print Odd Numbers for loop continue/src/Main.java
@@ -1,2 +1,11 @@
+//Problem 6: Print Odd Numbers (for loop + continue)
 public class Main {
+    public static void main(String[] args){
+                for(int i = 1; i<= 20; i++){
+            if(i%2 == 0){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
 }
