b02868ab25d06fdd04f8cfda6dd77c871a3ff944
Arman Hossen <145398391+armanhossen-dev@users.noreply.github.com>
2025-07-11 11:50:13 +0600

    java

---
+++ ArrayLIst/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        // ArrayList = A resizeable array that stores objects (autoboxing).
+        //             ArrayLists are fixed in size, but ArrayLists can change.
+        ArrayList<Integer> nums  = new ArrayList<>();
+    }
+}
