11f5eb58c649e49c1f30ddddb3d602e06a5e8ce8
Arman Hossen <2armanhossen@gmail.com>
2025-07-23 03:27:02 +0600

    java

---
+++ Register Student method overload/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        /*  A university wants to implement a system where students can be registered in differnet
+            ways. implement an overloaded method registereStudent.
+            - registerStudent(String name, int age): register a student with name and age
+            - registerStudent(String name, int age, String course): register a studnet for a specific course
+            - registerStudent(String name, int age, String course, String email): register a student with email verification.
+         */
+
+    }
+}
---
+++ Register Student method overload/src/Register.java
@@ -0,0 +1,5 @@
+public class Register {
+    String name, course, email;
+    int age;
+    
+}
