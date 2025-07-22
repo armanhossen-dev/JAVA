1ad430c1f537b13715a61309b27b2272c1610ab7
Arman Hossen <2armanhossen@gmail.com>
2025-07-23 03:47:35 +0600

    java

--- Register Student method overload/src/Main.java
+++ Register Student method overload/src/Main.java
@@ -1,3 +1,5 @@
+import javax.imageio.spi.RegisterableService;
+
 public class Main {
     public static void main(String[] args){
         /*  A university wants to implement a system where students can be registered in differnet
@@ -6,6 +8,13 @@ public class Main {
             - registerStudent(String name, int age, String course): register a studnet for a specific course
             - registerStudent(String name, int age, String course, String email): register a student with email verification.
          */
+        Register stu1 = new Register();
+        stu1.registerStudent("Arman", 22);
 
+        Register stu2 = new Register();
+        stu2.registerStudent("Ripon", 23, "PPS");
+
+        Register stu3 = new Register();
+        stu3.registerStudent("Kan", 30, "PPS", "ahr2024@gmail.com");
     }
 }
--- Register Student method overload/src/Register.java
+++ Register Student method overload/src/Register.java
@@ -1,5 +1,29 @@
+import java.time.temporal.TemporalAmount;
+
 public class Register {
     String name, course, email;
     int age;
-    
+
+    void registerStudent(String name,int age){
+        this.name = name;
+        this.age = age;
+        System.out.println(name + ", " + age);
+    }
+
+    void registerStudent(String name, int age, String course){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        System.out.println(name + ", " + age + ", " + course);
+    }
+
+    void registerStudent(String name, int age, String course, String email){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.email = email;
+        System.out.println(name + ", " + age + ", " + course + ", " + email);
+    }
+
+
 }
